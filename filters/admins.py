020bf955fd985fc6917b6fc38f6f682039343978
Mortistubea <155967262+Mortistubea@users.noreply.github.com>
2025-02-21 16:10:20 +0500

    Update admins.py

--- filters/admins.py
+++ filters/admins.py
@@ -1,7 +1,1 @@
-from data.config import ADMINS
-from aiogram.dispatcher.filters.filters import Filter
-from aiogram import types
 
-class IsAdmin(Filter):
-    async def check(self, message: types.Message) -> bool:
-        return message.from_user.id in ADMINS
